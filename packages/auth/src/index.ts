fbeaf9ef3def0fc045364d38ba61217ff99456c8
HeraldoArman <heraldoarman7@gmail.com>
2026-03-19 14:09:29 +0700

    initial commit

---
+++ packages/auth/src/index.ts
@@ -0,0 +1,21 @@
+import { db } from "@tk2-pkpl/db";
+import * as schema from "@tk2-pkpl/db/schema/auth";
+import { env } from "@tk2-pkpl/env/server";
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { nextCookies } from "better-auth/next-js";
+
+export const auth = betterAuth({
+  database: drizzleAdapter(db, {
+    provider: "pg",
+
+    schema: schema,
+  }),
+  trustedOrigins: [env.CORS_ORIGIN],
+  emailAndPassword: {
+    enabled: true,
+  },
+  secret: env.BETTER_AUTH_SECRET,
+  baseURL: env.BETTER_AUTH_URL,
+  plugins: [nextCookies()],
+});
